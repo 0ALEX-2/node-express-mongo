dbd105602a7aaa2fe6aa73e23bbbce1b3d400f20
0ALEX-2 <sakilahmed965@gmail.com>
2023-11-12 08:52:38 +0530

    templates

---
+++ natours/starter/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message:'Helo from the server side!',app:"Natures"})
+})
+
+app.listen(port, (err, data) => {
+    console.log("App is running on port 3000 ...");
+})
